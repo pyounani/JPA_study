b21e5113473f0f3a0cab47e67ecbc5f2e921ba71
pyounani <yndbsk9372@gmail.com>
2024-02-20 10:02:55 +0900

    [setup] 프로젝트 환경 설정

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+}
